89206556d20421da4eaea22f3e7c15c932a54393
Leonardo A Baranelli <leobaranelli@gmail.com>
2023-09-20 13:18:04 -0300

    feat: complete homework

--- FT-M1/01-IntroToCS/homework/homework.js
+++ FT-M1/01-IntroToCS/homework/homework.js
@@ -1,8 +1,26 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let decimal = 0;   
+   
+   for( let i = 0; i < num.length; i++){
+      decimal += parseInt(num[i]) * 2 ** (num.length -1 -i);      
+   }
 
-function DecimalABinario(num) {}
+   return decimal;
+
+}
+
+function DecimalABinario(num) {
+   const binary = [];   
+
+   while(num >= 1){
+      binary.push(num % 2);
+      num = Math.trunc(num / 2);
+   }
+
+   return binary.reverse().join("");
+}
 
 module.exports = {
    BinarioADecimal,
