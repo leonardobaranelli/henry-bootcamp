103c1ec7493ce2560ea576c9f6bec8967597b51c
Leonardo A Baranelli <leobaranelli@gmail.com>
2023-09-24 14:53:37 -0300

    feat: complete homework

--- FT-M3/01-Node/homework/01 - Exercises/commands/index.js
+++ FT-M3/01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,62 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd());
+}
 
-function date() {}
+function date(print) {
+    print(Date());
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args);
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir(".", (error, files) => {
+        if (error) throw Error("There was an error");
+        print(files.join(" "));
+    });
+}
 
-function cat() {}
+function cat(print, args) {
+    printFile(print, args);
+}
 
-function head() {}
+function head(print, args) {
+    printFile(print, args, "head");
+}
 
-function tail() {}
+function tail(print, args) {
+    printFile(print, args, "tail");
+}
 
-function curl() {}
+function curl(print, args) {
+    utils.request(`https://${args}`, (error, response) => {
+        if (error) throw new Error("There was an error");
+        print(response);
+    });
+}
 
-module.exports = {};
+const printFile = (print, filename, lines) => {
+    fs.readFile(filename, 'utf-8', (error, data) => {
+        if (error) throw Error("There was an error");
+        
+        !lines && print(data);
+        //lines === "head" && print(data.split("\n").slice(0,8).join("\n")); 
+        lines === "head" && print(data.split("\n")[0]);
+        lines === "tail" && print(data.split("\n").at(-1).trim());
+    });
+};
+
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail, 
+    curl,    
+};
