6cc1f66417054a8fb79c78e6f6d6ac8d58c8bea5
Leonardo A Baranelli <leobaranelli@gmail.com>
2023-09-24 14:52:00 -0300

    feat: complete exercise 1

--- FT-M3/01-Node/homework/01 - Exercises/bash.js
+++ FT-M3/01-Node/homework/01 - Exercises/bash.js
@@ -1,8 +1,22 @@
 const process = require('process');
-const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write("prompt > ");      
+   process.stdin.on("data", (data) => {
+      const aux = data.toString().trim().split(" ");
+      const cmd = aux.shift();
+      const args = aux.join(" ");         
+      
+      if (commands[cmd]) commands[cmd](print, args);
+      else print(`command not found: ${cmd}`);
+   });
+}
+
+function print(output) {
+   process.stdout.write(output);
+   process.stdout.write("\nprompt > ");
+}
 
 bash();
 module.exports = {
