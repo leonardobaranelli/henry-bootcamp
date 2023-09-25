79e3d315281f8036ba221a35f552391010ad4ac4
Leonardo A Baranelli <leobaranelli@gmail.com>
2023-09-25 09:00:20 -0300

    feat: complete homework

--- FT-M2/03-ES6/homework/src/selector.js
+++ FT-M2/03-ES6/homework/src/selector.js
@@ -9,6 +9,14 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if (matchFunc(startEl)) resultSet.push(startEl);
+
+  for (const child of startEl.children){
+    const result = traverseDomAndCollectElements(matchFunc, child);
+    resultSet = [...resultSet, ...result];
+  }
+
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -16,6 +24,11 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  if(selector[0] === "#") return "id";
+  else if(selector[0] === ".") return "class";
+  else if(selector.includes(".")) return "tag.class";
+
+  else return  "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -24,13 +37,33 @@ var selectorTypeMatcher = function (selector) {
 // matchea el selector.
 
 var matchFunctionMaker = function (selector) {
-  var selectorType = selectorTypeMatcher(selector);
+  var selectorType = selectorTypeMatcher(selector); 
   var matchFunction;
+
   if (selectorType === "id") {
+    matchFunction = (element) => {
+      if(`#${element.id}` === selector) return true;
+        return false;
+    }
   } else if (selectorType === "class") {
+      matchFunction = (element) => {
+        for( const _class of element.classList){
+          if( '.'+_class === selector) return true;
+        }
+        return false;
+      }
   } else if (selectorType === "tag.class") {
+    matchFunction = (element) => {
+      const [tag, className] = selector.split(".");
+
+      return matchFunctionMaker(tag)(element) && matchFunctionMaker(`.${className}`)(element);
+    }
   } else if (selectorType === "tag") {
-  }
+    matchFunction = (element) => {
+      if(element.tagName.toLowerCase() === selector) return true;
+        return false;
+    }
+  }  
   return matchFunction;
 };
 
@@ -38,5 +71,6 @@ var $ = function (selector) {
   var elements;
   var selectorMatchFunc = matchFunctionMaker(selector);
   elements = traverseDomAndCollectElements(selectorMatchFunc);
+  
   return elements;
 };
